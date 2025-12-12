027c5c03f66f3ac892c350e306dd2bb6bd16b069
talli1119 <talli96123@gmail.com>
2025-12-12 22:06:25 +0800

    feat: add DATABASE_URL to environment configuration and update dependencies for dotenv

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,13 +1,15 @@
 from sqlalchemy import Column, String, create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 Base = declarative_base()
-DATABASE_URL = "postgresql://ai_paper_partner:password@localhost:5432/ai_paper_partner_db"
 engine = create_engine(DATABASE_URL,echo=True)
 DBSession = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-session = DBSession()
 
 def get_db():
     db = DBSession()
